99b1d763239569496e12bdf29f0e83c3b3766e8a
yogendrasgautam <yogendrasgautam@gmail.com>
2021-10-08 11:56:10 +0530

    sample commit 1

--- src/services/catalog/catalog.api/Startup.cs
+++ src/services/catalog/catalog.api/Startup.cs
@@ -33,6 +33,8 @@ namespace catalog.api
             });
         }
 
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
